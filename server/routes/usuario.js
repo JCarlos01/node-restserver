f34f35f021bbd50f590a86fde67f4dd950ca9161
JuanC <carlosgomv@gmail.com>
2020-05-16 13:49:19 -0500

    Login Normal seccion 10

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,14 +1,18 @@
 const express = require('express');
 
 const bcrypt = require('bcrypt');
-
 const _ = require('underscore');
 
 const Usuario = require('../models/usuario');
+const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion')
 
 const app = express();
 
-app.get('/usuario', function(req, res) {
+app.get('/usuario', verificaToken, (req, res) => {
+
+
+
+
 
     let desde = req.query.desde || 0;
     desde = Number(desde);
@@ -44,7 +48,7 @@ app.get('/usuario', function(req, res) {
 
 });
 
-app.post('/usuario', function(req, res) {
+app.post('/usuario', [verificaToken, verificaAdmin_Role], function(req, res) {
 
     let body = req.body;
 
@@ -72,12 +76,10 @@ app.post('/usuario', function(req, res) {
     })
 });
 
-app.put('/usuario/:id', function(req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], function(req, res) {
 
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
-
-
 
     Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
 
@@ -87,20 +89,14 @@ app.put('/usuario/:id', function(req, res) {
                 err
             })
         }
-
-
-
         res.json({
             ok: true,
             usuario: usuarioDB
         })
-
-
     })
-
 });
 
-app.delete('/usuario/:id', function(req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdmin_Role], function(req, res) {
 
     let id = req.params.id;
 
