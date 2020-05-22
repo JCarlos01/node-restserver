4566aec73a0e63f8a70157b48601ca0c44b767e1
JuanC <carlosgomv@gmail.com>
2020-05-21 20:40:06 -0500

    pruebados

---
+++ server/routes/upload.js
@@ -0,0 +1,118 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const app = express();
+
+const Usuario = require('../models/usuario');
+
+// default options
+app.use(fileUpload({ seTempFiles: true }));
+
+app.put('/upload/:tipo/:id', function(req, res) {
+
+    let tipo = req.params.tipo;
+    let id = req.params.id;
+
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400)
+            .json({
+                ok: false,
+                err: {
+                    message: 'No se ha seleccionado ningún archivo'
+                }
+            });
+    }
+
+    // Validar tipo
+
+    let tiposValidos = ['productos', 'usuarios'];
+    if (tiposValidos.indexOf(tipo) < 0) {
+
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'Las tipos permitidos son ' + tiposValidos.join(', '),
+
+            }
+        });
+
+    }
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    let archivo = req.files.archivo;
+    let nombreCoratdo = archivo.name.split('.');
+    let extension = nombreCoratdo[nombreCoratdo.length - 1];
+
+    // Extensiones permitidas
+    let extensionesValidas = ['png', 'jpg', 'gif', 'jpeg'];
+
+    if (extensionesValidas.indexOf(extension) < 0) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'Las extensiones permitidas son ' + extensionesValidas.join(', '),
+                ext: extension
+            }
+        });
+    }
+
+    // Cambiar nombre al archivo
+    let nombreArchivo = `${ id }-${ new Date().getMilliseconds() }.${ extension }`
+
+    archivo.mv(`uploads/${ tipo }/${ nombreArchivo }`, (err) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        //Aqui, imagen cargada
+
+        imagenUsuario(id, res, nombreArchivo);
+
+    });
+});
+
+
+function imagenUsuario(id, res, nombreArchivo) {
+
+    Usuario.findById(id, (err, usuarioDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!usuarioDB) {
+            return res.status(500).json({
+                ok: false,
+                err: {
+                    message: 'Usuario no existe'
+                }
+            });
+        }
+
+        usuarioDB.img = nombreArchivo;
+
+        usuarioDB.save((err, usuarioGuardado) => {
+
+            res.json({
+                ok: true,
+                usuario: usuarioGuardado,
+                img: nombreArchivo
+            });
+
+        });
+
+
+    })
+
+
+}
+
+function imagenProducto() {
+
+}
+
+module.exports = app;
